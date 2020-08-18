0a48a0b94ac23de1efed9c62724a8a88fe519f6d
Raghav <raghavthakar12@gmail.com>
2020-08-18 11:33:17 +0530

    Eabled LaserScan and Camera

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/raghav/IEEE_sample_bot/src
+# Build directory: /home/raghav/IEEE_sample_bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("description")
+subdirs("control")
+subdirs("simulation")
